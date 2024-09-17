e201cdf2c36e9c8f7bb58a6dd6c0620808d43f7b
MariaBelenNunezFranco <138376911+MariaBelenNunezFranco@users.noreply.github.com>
2024-09-17 19:18:58 +0200

    Antes de actualizar la version de angular

--- src/app/studies/studies.component.ts
+++ src/app/studies/studies.component.ts
@@ -11,6 +11,21 @@ export class StudiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let next = document.querySelector('.next');
+    let prev = document.querySelector('.prev');
+    let slider = document.querySelector('.slider');
+
+    next?.addEventListener('click', function () {
+      let slides = document.querySelectorAll('.slides');
+      slider?.appendChild(slides[0]);
+    })
+
+    prev?.addEventListener('click', function () {
+      let slides = document.querySelectorAll('.slides');
+      slider?.prepend(slides[slides.length - 1]);
+    })
+
+
   }
 
 }
